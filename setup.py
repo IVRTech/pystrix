66b41cafe5a1ffeabff7f93e25e3fd37923baea4
Neil Tallim <flan@uguu.ca>
2011-12-05 09:57:17 -0700

    Fixed a typo.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 setup(
  name = 'pystrix',
  version = '0.9.1',
- description = 'Python bindings for Asterisk Manager Interface and Asterist Gateway Interface',
+ description = 'Python bindings for Asterisk Manager Interface and Asterisk Gateway Interface',
  author = 'Neil Tallim',
  author_email = 'neil.tallim@linux.com',
  license = 'LGPLv3',
