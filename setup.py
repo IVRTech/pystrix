97e31ffdce5a1db2283d2f00dcb4a46652459319
Marta Solano <marta.solano@ivrtechnology.com>
2017-02-09 12:25:56 -0600

    Update setup to upload package to pypi.

--- setup.py
+++ setup.py
@@ -8,18 +8,37 @@ from distutils.core import setup
 
 from pystrix import VERSION
 
-setup(
- name = 'pystrix',
- version = VERSION,
- description = 'Python bindings for Asterisk Manager Interface and Asterisk Gateway Interface',
- author = 'Neil Tallim',
- author_email = 'neil.tallim@linux.com',
- license = 'LGPLv3',
- url = 'http://code.google.com/p/media-storage/',
- packages = [
-  'pystrix',
-  'pystrix.agi',
-  'pystrix.ami',
-  ],
-)
+from setuptools import setup, find_packages
+import os
 
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Communications :: Telephony'
+]
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    author='Marta S.',
+    author_email='marsoguti@gmail.com',
+    name='pystrix',
+    version=VERSION,
+    description='Python bindings for Asterisk Manager Interface and Asterisk Gateway Interface',
+    long_description=README,
+    url='https://github.com/marsoguti/pystrix',
+    license='GNU General Public License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    packages=[
+     'pystrix',
+     'pystrix.agi',
+     'pystrix.ami',
+    ]
+)
