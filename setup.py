c072086f75714e39b64b62725626fe84c61d4fd4
Marta Solano <marta.solano@ivrtechnology.com>
2017-04-18 19:12:30 -0600

    Update setup.

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ CLASSIFIERS = [
     'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2.6+',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
     'Topic :: Communications :: Telephony'
 ]
 
